e55183319e2b91a19371d27fa737d21617cabf8d
Kirill S. <kirr2@gf.tom.ru>
2016-04-30 00:14:06 +0600

    Release 1.0.0.0: Variant 2 - without booster;

--- WPFBoosterPick/MainWindow.xaml.cs
+++ WPFBoosterPick/MainWindow.xaml.cs
@@ -14,17 +14,8 @@ namespace WPFBoosterPick
         public MainWindow()
         {
             InitializeComponent();
-            (Resources["AnimBooster"] as Storyboard).Completed += new EventHandler(Refresh_anim);
         }
 
-        private void Refresh_anim(object sender, EventArgs e)
-        {
-            (Resources["AnimBooster"] as Storyboard).Begin();
-        }
-        private void Refresh_Pulse()
-        {
-            (Resources["AnimPulse"] as Storyboard).Begin();
-        }
         private void Refresh_Logo()
         {
             (Resources["LightLogo"] as Storyboard).Begin();
@@ -32,28 +23,28 @@ namespace WPFBoosterPick
 
         private void button_plus_epic_Click(object sender, RoutedEventArgs e)
         {
-            Refresh_Pulse();
+            //Refresh_Logo(); //Если хочешь для каждого анимацию, то добавь в каждый метод; По-умолчанию анимация только у золотых карт и у простых легендарок.
             db.epic_count++;
             EpicCount.Text = db.epic_count.ToString();
         }
 
         private void button_plus_leg_Click(object sender, RoutedEventArgs e)
         {
-            Refresh_Pulse();
+            Refresh_Logo();
             db.leg_count++;
             LegCount.Text = db.leg_count.ToString();
         }
 
         private void button_plus_gold_com_Click(object sender, RoutedEventArgs e)
         {
-            Refresh_Pulse();
+            Refresh_Logo();
             db.g_com_count++;
             GoldComCount.Text = db.g_com_count.ToString();
         }
 
         private void button_plus_gold_rare_Click(object sender, RoutedEventArgs e)
         {
-            Refresh_Pulse();
+            Refresh_Logo();
             db.g_rare_count++;
             GoldRareCount.Text = db.g_rare_count.ToString();
         }
