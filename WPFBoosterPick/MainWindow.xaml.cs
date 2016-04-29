f558b065dbd2c3d325f34c6396ed56c64c74011a
Kirill S <skalix@gf.tom.ru>
2016-04-29 12:26:59 +0600

    Check point;

--- WPFBoosterPick/MainWindow.xaml.cs
+++ WPFBoosterPick/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,18 @@ namespace WPFBoosterPick
         public MainWindow()
         {
             InitializeComponent();
+            (Resources["AnimImg"] as Storyboard).Completed += new EventHandler(Refresh_anim);
+            (Resources["AnimPulse"] as Storyboard).Completed += Pulse_Completed;
+        }
+
+        private void Pulse_Completed(object sender, EventArgs e)
+        {
+            (Resources["AnimPulse"] as Storyboard).Begin();
+        }
+
+        private void Refresh_anim(object sender, EventArgs e)
+        {
+            (Resources["AnimImg"] as Storyboard).Begin();
         }
     }
 }
