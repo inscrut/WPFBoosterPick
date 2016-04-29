af3c24c19d75eba8e8ce319b22d0c192c1ffdc96
Kirill S. <kirr2@gf.tom.ru>
2016-04-29 23:00:03 +0600

    Check point;

---
+++ WPFBoosterPick/DB.cs
@@ -0,0 +1,43 @@
+﻿namespace WPFBoosterPick
+{
+    public class DB
+    {
+        /// <summary>
+        /// Колличество эпических карт
+        /// </summary>
+        public int epic_count { get; set; }
+        /// <summary>
+        /// Колличество легендарных карт
+        /// </summary>
+        public int leg_count { get; set; }
+        /// <summary>
+        /// Колличество золотых обычных карт
+        /// </summary>
+        public int g_com_count { get; set; }
+        /// <summary>
+        /// Колличество золотых редких карт
+        /// </summary>
+        public int g_rare_count { get; set; }
+        /// <summary>
+        /// Колличество золотых эпических карт
+        /// </summary>
+        public int g_epic_count { get; set; }
+        /// <summary>
+        /// Колличество золотых легендарных карт
+        /// </summary>
+        public int g_leg_count { get; set; }
+
+        /// <summary>
+        /// Создать пустой класс
+        /// </summary>
+        public DB()
+        {
+            epic_count = 0;
+            leg_count = 0;
+            g_com_count = 0;
+            g_rare_count = 0;
+            g_epic_count = 0;
+            g_leg_count = 0;
+        }
+    }
+}
